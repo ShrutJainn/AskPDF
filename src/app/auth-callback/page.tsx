b5b5325827dc33972245b5c528b4def526e7f4d9
ShrutJainn <shrutjain488@gmail.com>
2024-07-10 21:33:21 +0530

    Added tRPC

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,9 +1,16 @@
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
 function Page() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
+
+  const { data } = trpc.authCallback.useQuery(undefined);
+  if (data) {
+    //user is in the database
+    router.push(origin ? `/${origin}` : "/dashboard");
+  }
 }
 
 export default Page;
